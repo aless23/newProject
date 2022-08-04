7f96820320a0a135a814c784992cede819879c72
Oleg Lukash <justifyaless23@gmail.com>
2022-08-04 02:06:22 +0200

    create Js project Movies st1

--- scriptjs.js
+++ scriptjs.js
@@ -1,7 +1,27 @@
 "use strict";
 
-function summa (a , b){
-    return a + b;
-}
+let answer = prompt('Сколько фильмов вы посмотрели?');
 
-console.log(`Summa ravna ${summa(3, 4)}`);
+const perosonalMovieDb = {
+count: answer,
+movies:{},
+actors: {},
+genres: [],
+privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ""),
+    b = prompt ('На сколько оцените его?', ""),
+    c = prompt('Один из последних просмотренных фильмов', ""),
+    d = prompt ('На сколько оцените его?', "");
+
+    perosonalMovieDb.movies[a] = b;
+    perosonalMovieDb.movies[c] = d;
+
+    console.log(perosonalMovieDb);
+
+
+// Разновидность функций
+// function declaration: function example (){ code } создаётся до начала выполнения скрипта, можна вызвать перед объявлением
+// function expression: let example = function (){ code }; создаётся только тогда, когда доходит поток кода, можно вызвать только после объявления
+// стрелочные функции: () = > не имеет своего контекста (this)
